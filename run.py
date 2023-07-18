ab6d6f12cf7708882c2884080f19688793f0449b
iris-ywang <iris_ywang@outlook.com>
2023-07-18 10:14:20 +0100

    run a new set of 9 datasets, 25 batch runs per data, rank_only(for HPC)

--- run.py
+++ run.py
@@ -27,9 +27,9 @@ def run_single_dataset(dataset_filename, dataset_shuffle_state=None):
 
     # only one of them can be True.
     logging.info(f"Dataset name: {dataset_filename}")
-    rank_only = False
+    rank_only = True
     uncertainty_only = False
-    ucb = True
+    ucb = False
     proportion_leave_out_test = 0.15  # 0 - 1
     logging.info(f"Acquisition function setting: "
                  f"rank_only - {rank_only}, "
